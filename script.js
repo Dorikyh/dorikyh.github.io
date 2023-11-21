02fe36c72d5ede3864bca4631e249a7c1e0664b0
Fabián Márquez <80162941+Dorikyh@users.noreply.github.com>
2023-11-21 15:42:41 -0500

    Updated multilanguage system

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-// Obtener el idioma del navegador del usuario
-var userLang = navigator.language || navigator.userLanguage;
+// Obtener el idioma del navegador del usuario y convertirlo a minúsculas para la comparación
+var userLang = (navigator.language || navigator.userLanguage).toLowerCase();
 
-// Redirigir a la página correspondiente al idioma del navegador
-if (userLang === 'es') {
-  window.location.href = 'index_es.html';
-} else if (userLang === 'fr') {
-  window.location.href = 'index_fr.html';
+// Verificar si el idioma contiene subcadenas específicas
+if (userLang.includes('es')) {
+  window.location.href = 'index_es.html'; // Redirigir a la página en español
+} else if (userLang.includes('fr')) {
+  window.location.href = 'index_fr.html'; // Redirigir a la página en francés
+} else if (userLang.includes('en')) {
+  window.location.href = 'index_en.html'; // Redirigir a la página en inglés
+} else {
+  // Redirigir a una página predeterminada en caso de no coincidir con los idiomas deseados
+  window.location.href = 'index.html';
 }
